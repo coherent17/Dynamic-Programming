4f48a5afc0c3c8fa739b22259793a80b5b4093b1
coherent17 <mnb51817@gmail.com>
2022-01-20 02:40:32 +0800

    add base case

--- ClimbeStairs/ClimbeStairs.c
+++ ClimbeStairs/ClimbeStairs.c
@@ -3,10 +3,13 @@
 
 int climbStairs(int n){
     int *dp = malloc(sizeof(int) * (n + 1));
+
+    //base case
     dp[0] = 1;
     dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= n;i++){
+
+    //inductive to find answer
+    for (int i = 2; i <= n;i++){
         dp[i] = dp[i - 1] + dp[i - 2];
     }
     int result = dp[n];
